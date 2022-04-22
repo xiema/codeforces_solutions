87409b799b11724a40fb7bcc49653b95002ebe44
xiema <maxprincipe@yahoo.com>
2022-04-22 16:50:56 +0800

    allow passing workdir as arg

--- check_python.py
+++ check_python.py
@@ -1,11 +1,15 @@
 from os import scandir
 import re
+import sys
 from subprocess import run
 
-solution = None
-for fe in scandir():
+workdir = sys.argv[1] if len(sys.argv) > 1 else None
+testsdir = f"{workdir}/tests" if workdir else "tests"
+
+solution, solution_path = None, None
+for fe in scandir(workdir):
     if fe.is_file() and fe.name.endswith('.py') and not fe.name.startswith('_'):
-        solution = fe.name
+        solution, solution_path = fe.name, fe.path
         break
 
 if solution is None:
@@ -13,7 +17,7 @@ if solution is None:
     exit()
 
 tests = []
-for fe in scandir("tests"):
+for fe in scandir(testsdir):
     if fe.is_file() and fe.name.endswith('.in'):
         tests.append(fe.name[:-3])
 
@@ -23,17 +27,17 @@ if not tests:
 
 for testname in tests:
     ok = True
-    with open(f"tests/{testname}.in") as f:
-        subproc = run([solution], stdin=f, text=True, shell=True, capture_output=True)
+    with open(f"{testsdir}/{testname}.in") as f:
+        subproc = run([solution_path], stdin=f, text=True, shell=True, capture_output=True)
     if subproc.returncode != 0:
         print(subproc.stderr)
         continue
     result = subproc.stdout.splitlines(keepends=True)
-    with open(f"tests/{testname}.out") as f:
+    with open(f"{testsdir}/{testname}.out") as f:
         answer = f.readlines()
         for i in range(len(answer)):
             if i < len(result):
-                if result[i] != answer[i]:
+                if result[i].strip() != answer[i].strip():
                     ok = False
                     print(f"LINE {i+1}\n\t{answer[i]}\n\t{result[i]}")
             else:
